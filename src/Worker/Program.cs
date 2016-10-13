d75a9b44c9394f4b78dc9c9500d5c52bba6aa70a
Marc Villacorta <marc.villacorta@gmail.com>
2016-10-13 11:27:30 +0200

    Target the right redis service port

--- src/Worker/Program.cs
+++ src/Worker/Program.cs
@@ -17,7 +17,7 @@ namespace Worker
             try
             {
                 var pgsql = OpenDbConnection("Server=marathon-lb;Port=10014;Username=postgres;");
-                var redis = OpenRedisConnection("marathon-lb").GetDatabase();
+                var redis = OpenRedisConnection("marathon-lb:10012").GetDatabase();
 
                 var definition = new { vote = "", voter_id = "" };
                 while (true)
